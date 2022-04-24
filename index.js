637c21b29f9791aa75e53dab73551b05704d444c
masud1989 <mranasrj@gmail.com>
2022-04-24 14:28:09 +0600

    data-loding-from server

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const app = express();
+
+//Middleware===================================
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.p2ek9.mongodb.net/ttt?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+        const productCollection = client.db('emaJohn').collection('product');
+
+        app.get('/product', async(req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+    }
+    finally{
+
+    }
+} 
+run().catch(console.dir)
+
+
+
+app.get('/', (req, res) => {
+    res.send('Server is Running..');
+})
+
+app.listen(port, () =>{
+    console.log('Server is Running on port', port);
+})
